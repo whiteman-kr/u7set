50623bf8d9e72377e311897abf1b19684d74a6ae
Boris Vintenko <boris_vin@ukr.net>
2015-03-23 14:00:44 +0200

    u7: js script, tabs replaced by spaces

--- u7/LogicModuleConfiguration.js
+++ u7/LogicModuleConfiguration.js
@@ -45,9 +45,9 @@ function module_lm_1(device, confCollection, log)
     {
         var child = device.jsChild(i);
         if (module_lm_1(child, confCollection, log) == false)
-		{
-			return false;
-		}
+        {
+            return false;
+        }
     }
 
     return true;
@@ -89,71 +89,71 @@ function generate_lm_1_rev3(module, confCollection, log)
     for (var i = 0; i < parent.childrenCount(); i++)
     {
         var ioModule = parent.jsChild(i);
-		if (ioModule.ConfType == "AIM" || ioModule.ConfType == "AIFM" || ioModule.ConfType == "AOM" || ioModule.ConfType == "OCM")
-		{
-			var frame = ioModulesStartFrame + ioModule.Place - 1;
-			if (frame < ioModulesStartFrame || frame >= ioModulesStartFrame + ioModulesMaxCount)
-			{
-				log.writeMessage("Wrong I/O module place: " + ioModule.StrID + ", place: " + ioModule.Place + ", expected 1..14.");
-				return false;
-			}
-			
-			if (ioModule.ConfType == "AIM")
-			{
-				generate_aim(confFirmmware, ioModule, frame, log);
-			}
-			if (ioModule.ConfType == "AIFM")
-			{
-				generate_aifm(confFirmmware, ioModule, frame, log);
-			}
-			if (ioModule.ConfType == "AOM")
-			{
-				generate_aom(confFirmmware, ioModule, frame, log);
-			}
-			if (ioModule.ConfType == "OCM")
-			{
-				generate_ocm(confFirmmware, ioModule, frame, log);
-			}
-		}
-	}
-	
-	// Create TxRx Blocks (Opto) configuration
-	//
-	var txRxOptoCount = 3;
-	var txRxOptoStartFrame = 16;
-	
+        if (ioModule.ConfType == "AIM" || ioModule.ConfType == "AIFM" || ioModule.ConfType == "AOM" || ioModule.ConfType == "OCM")
+        {
+            var frame = ioModulesStartFrame + ioModule.Place - 1;
+            if (frame < ioModulesStartFrame || frame >= ioModulesStartFrame + ioModulesMaxCount)
+            {
+                log.writeMessage("Wrong I/O module place: " + ioModule.StrID + ", place: " + ioModule.Place + ", expected 1..14.");
+                return false;
+            }
+            
+            if (ioModule.ConfType == "AIM")
+            {
+                generate_aim(confFirmmware, ioModule, frame, log);
+            }
+            if (ioModule.ConfType == "AIFM")
+            {
+                generate_aifm(confFirmmware, ioModule, frame, log);
+            }
+            if (ioModule.ConfType == "AOM")
+            {
+                generate_aom(confFirmmware, ioModule, frame, log);
+            }
+            if (ioModule.ConfType == "OCM")
+            {
+                generate_ocm(confFirmmware, ioModule, frame, log);
+            }
+        }
+    }
+    
+    // Create TxRx Blocks (Opto) configuration
+    //
+    var txRxOptoCount = 3;
+    var txRxOptoStartFrame = 16;
+    
     for (var i = 0; i < txRxOptoCount; i++)
     {
-		var ptr = 0;
-		confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 10);		//Start address
-		ptr += 2;
-		confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 20);			//Quantity of words
-		ptr += 2;
-		confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 30);			//Tx ID
-		ptr += 2;
-		confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 40);			//Quantity of words
-		ptr += 2;
-		//reserved
-		ptr += 1008;
-		confFirmmware.storeCrc64(txRxOptoStartFrame + i, 0, ptr, ptr);	//CRC-64
-		ptr += 8;
-	}
-	
+        var ptr = 0;
+        confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 10);       //Start address
+        ptr += 2;
+        confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 20);           //Quantity of words
+        ptr += 2;
+        confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 30);           //Tx ID
+        ptr += 2;
+        confFirmmware.setData16(txRxOptoStartFrame + i, ptr, 40);           //Quantity of words
+        ptr += 2;
+        //reserved
+        ptr += 1008;
+        confFirmmware.storeCrc64(txRxOptoStartFrame + i, 0, ptr, ptr);  //CRC-64
+        ptr += 8;
+    }
+    
     // Create LANs configuration
-	//
-	var lanCount = 3;
-	var lanStartFrame = 19;
-	
+    //
+    var lanCount = 3;
+    var lanStartFrame = 19;
+    
     for (var i = 0; i < lanCount; i++)
     {
-		var ptr = 0;
-		//confFirmmware.setData16(lanStartFrame + i, ptr, 10);		//LAN configuration
-		ptr += 62;
-		//reserved
-		ptr += 954;
-		confFirmmware.storeCrc64(lanStartFrame + i, 0, ptr, ptr);	//CRC-64
-		ptr += 8;
-	}
+        var ptr = 0;
+        //confFirmmware.setData16(lanStartFrame + i, ptr, 10);      //LAN configuration
+        ptr += 62;
+        //reserved
+        ptr += 954;
+        confFirmmware.storeCrc64(lanStartFrame + i, 0, ptr, ptr);   //CRC-64
+        ptr += 8;
+    }
 
     return true;
 }
@@ -165,42 +165,42 @@ function generate_lm_1_rev3(module, confCollection, log)
 //
 function generate_aim(confFirmmware, module, frame, log)
 {
-	log.writeMessage("AIM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
+    log.writeMessage("AIM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
 
-	var ptr = 0;
+    var ptr = 0;
 
-	// I/O Module configuration
-	//
-	// i/o module data... 640 bytes
-	ptr += 640;
-	
-	confFirmmware.storeCrc64(frame, 0, ptr, ptr);	//CRC-64
-	ptr += 8;
+    // I/O Module configuration
+    //
+    // i/o module data... 640 bytes
+    ptr += 640;
+    
+    confFirmmware.storeCrc64(frame, 0, ptr, ptr);   //CRC-64
+    ptr += 8;
 
-	//	Flags word
-	//
-	var configEnableFlag = true;
-	var dataEnableFlag = true;
-	var dataReceiveEnableFlag = true;
-	
-	var flags = 0;
-	if (configEnableFlag == true)
-		flags |= 1;
-	if (dataEnableFlag == true)
-		flags |= 2;
-	if (dataReceiveEnableFlag == true)
-		flags |= 4;
-	
-	generate_txRxConfig(confFirmmware, frame, ptr, flags, log);
-	ptr += 12;
-	
-	//reserved
-	ptr += 356;
+    //  Flags word
+    //
+    var configEnableFlag = true;
+    var dataEnableFlag = true;
+    var dataReceiveEnableFlag = true;
+    
+    var flags = 0;
+    if (configEnableFlag == true)
+        flags |= 1;
+    if (dataEnableFlag == true)
+        flags |= 2;
+    if (dataReceiveEnableFlag == true)
+        flags |= 4;
+    
+    generate_txRxConfig(confFirmmware, frame, ptr, flags, log);
+    ptr += 12;
+    
+    //reserved
+    ptr += 356;
 
-	confFirmmware.storeCrc64(frame, 0, ptr, ptr);	//CRC-64
-	ptr += 8;
-	
-	return true;
+    confFirmmware.storeCrc64(frame, 0, ptr, ptr);   //CRC-64
+    ptr += 8;
+    
+    return true;
 }
 
 // Generate configuration for module AIFM
@@ -210,8 +210,8 @@ function generate_aim(confFirmmware, module, frame, log)
 //
 function generate_aifm(confFirmmware, module, frame, log)
 {
-	log.writeMessage("AIFM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
-	return true;
+    log.writeMessage("AIFM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
+    return true;
 
 }
 
@@ -222,8 +222,8 @@ function generate_aifm(confFirmmware, module, frame, log)
 //
 function generate_aom(confFirmmware, module, frame, log)
 {
-	log.writeMessage("AOM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
-	return true;
+    log.writeMessage("AOM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
+    return true;
 
 }
 
@@ -234,25 +234,25 @@ function generate_aom(confFirmmware, module, frame, log)
 //
 function generate_ocm(confFirmmware, module, frame, log)
 {
-	log.writeMessage("AIFM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
-	return true;
+    log.writeMessage("AIFM: " + module.StrID + " Place: " + module.Place + " Frame: " + frame);
+    return true;
 
 }
 
 function generate_txRxConfig(confFirmmware, frame, offset, flags, log)
 {
-	// TxRx Block's configuration structure
-	//
-	var ptr = offset;
-	
-	confFirmmware.setData16(frame, ptr, flags);		// Flags word
-	ptr += 2;
-	confFirmmware.setData16(frame, ptr, 24);		// Configuration words quantity
-	ptr += 2;
-	confFirmmware.setData16(frame, ptr, 24);		// Data words quantity
-	ptr += 2;
-	confFirmmware.setData16(frame, ptr, 24);		// Tx ID
-	ptr += 2;
-	
-	return true;
+    // TxRx Block's configuration structure
+    //
+    var ptr = offset;
+    
+    confFirmmware.setData16(frame, ptr, flags);     // Flags word
+    ptr += 2;
+    confFirmmware.setData16(frame, ptr, 24);        // Configuration words quantity
+    ptr += 2;
+    confFirmmware.setData16(frame, ptr, 24);        // Data words quantity
+    ptr += 2;
+    confFirmmware.setData16(frame, ptr, 24);        // Tx ID
+    ptr += 2;
+    
+    return true;
 }
