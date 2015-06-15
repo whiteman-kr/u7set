4e6c3037243a85de423f2d121fe6b1e02274821e
Boris Vintenko <boris_vin@ukr.net>
2015-06-15 14:35:14 +0300

    RPCT-360, u7: subsystem list editor automatically checks out and checks in the data file

--- u7/DialogSubsystemListEditor.cpp
+++ u7/DialogSubsystemListEditor.cpp
@@ -1,6 +1,7 @@
 #include "DialogSubsystemListEditor.h"
 #include "ui_DialogSubsystemListEditor.h"
 #include <QMessageBox>
+#include <QInputDialog>
 
 //
 //
@@ -20,6 +21,7 @@ QWidget* EditorDelegate::createEditor(QWidget *parent, const QStyleOptionViewIte
 	}
 	return nullptr;
 }
+
 
 //
 //
@@ -50,8 +52,8 @@ DialogSubsystemListEditor::DialogSubsystemListEditor(DbController *pDbController
 	ui->m_list->setColumnWidth(2, 100);
 	ui->m_list->setColumnWidth(3, 150);
 
-	EditorDelegate *d = new EditorDelegate(this);
-	ui->m_list->setItemDelegate(d);
+	m_editorDelegate = new EditorDelegate(this);
+	ui->m_list->setItemDelegate(m_editorDelegate);
 
 	QString errorCode;
 
@@ -87,6 +89,95 @@ DialogSubsystemListEditor::~DialogSubsystemListEditor()
 	delete ui;
 }
 
+bool DialogSubsystemListEditor::askForSaveChanged()
+{
+	if (m_modified == false)
+	{
+		return true;
+	}
+
+	QMessageBox::StandardButton result = QMessageBox::warning(this, "Subsystem List Editor", "Do you want to save your changes?", QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+
+	if (result == QMessageBox::Yes)
+	{
+		if (saveChanges() == false)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	if (result == QMessageBox::No)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool DialogSubsystemListEditor::saveChanges()
+{
+	bool ok;
+	QString comment = QInputDialog::getText(this, tr("Subsystem List Editor"),
+											tr("Please enter comment:"), QLineEdit::Normal,
+											tr("comment"), &ok);
+
+	if (ok == false)
+	{
+		return false;
+	}
+	if (comment.isEmpty())
+	{
+		QMessageBox::warning(this, "Subsystem List Editor", "No comment supplied!");
+		return false;
+	}
+
+	Hardware::SubsystemStorage subsystems;
+
+	for (int i = 0; i < ui->m_list->topLevelItemCount(); i++)
+	{
+		QTreeWidgetItem* item = ui->m_list->topLevelItem(i);
+		if (item == nullptr)
+		{
+			assert(item);
+			return false;
+		}
+
+		int index = item->data(0, Qt::UserRole).toInt();
+		int key = item->text(1).toInt();
+		QString strId = item->text(2);
+		QString caption = item->text(3);
+
+		std::shared_ptr<Hardware::Subsystem> subsystem = std::make_shared<Hardware::Subsystem>(index, key, strId, caption);
+		subsystems.add(subsystem);
+	}
+
+
+	// save to db
+	//
+	if (subsystems.save(db(), comment) == false)
+	{
+		QMessageBox::critical(this, QString("Error"), tr("Can't save subsystems."));
+		return false;
+	}
+
+	m_modified = false;
+
+	return true;
+}
+
+void DialogSubsystemListEditor::closeEvent(QCloseEvent* e)
+{
+	if (askForSaveChanged() == true)
+	{
+		e->accept();
+	}
+	else
+	{
+		e->ignore();
+	}
+}
+
 void DialogSubsystemListEditor::on_m_add_clicked()
 {
 	int index = -1;
@@ -119,6 +210,8 @@ void DialogSubsystemListEditor::on_m_add_clicked()
 	ui->m_list->clearSelection();
 	ui->m_list->selectionModel()->select(ui->m_list->model()->index (index, 0), QItemSelectionModel::Select | QItemSelectionModel::Rows);
 
+	m_modified = true;
+
 }
 
 void DialogSubsystemListEditor::on_m_remove_clicked()
@@ -147,40 +240,43 @@ void DialogSubsystemListEditor::on_m_remove_clicked()
 	}
 
 	ui->m_list->selectionModel()->select(ui->m_list->model()->index (index, 0), QItemSelectionModel::Select | QItemSelectionModel::Rows);
+
+	m_modified = true;
 }
 
-void DialogSubsystemListEditor::on_DialogSubsystemListEditor_accepted()
-{
-	Hardware::SubsystemStorage subsystems;
-
-	for (int i = 0; i < ui->m_list->topLevelItemCount(); i++)
-	{
-		QTreeWidgetItem* item = ui->m_list->topLevelItem(i);
-		if (item == nullptr)
-		{
-			assert(item);
-			return;
-		}
-
-		int index = item->data(0, Qt::UserRole).toInt();
-		int key = item->text(1).toInt();
-		QString strId = item->text(2);
-		QString caption = item->text(3);
-
-		std::shared_ptr<Hardware::Subsystem> subsystem = std::make_shared<Hardware::Subsystem>(index, key, strId, caption);
-		subsystems.add(subsystem);
-	}
-
-	// save to db
-	//
-	if (subsystems.save(db()) == false)
-	{
-		QMessageBox::critical(this, QString("Error"), tr("Can't save subsystems."));
-		return;
-	}
-}
 
 DbController* DialogSubsystemListEditor::db()
 {
 	return m_dbController;
+}
+
+
+void DialogSubsystemListEditor::on_buttonOk_clicked()
+{
+	if (m_modified == true)
+	{
+		if (saveChanges() == false)
+		{
+			return;
+		}
+	}
+
+	accept();
+	return;
+}
+
+void DialogSubsystemListEditor::on_buttonCancel_clicked()
+{
+	if (askForSaveChanged() == true)
+	{
+		reject();
+	}
+	return;
+}
+
+void DialogSubsystemListEditor::on_m_list_itemChanged(QTreeWidgetItem *item, int column)
+{
+	Q_UNUSED(item);
+	Q_UNUSED(column);
+	m_modified = true;
 }
