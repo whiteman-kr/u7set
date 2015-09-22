2ed32c2f1269c78c5b4ad0c3887c60d5ca265f20
Tkachuk Roman <katranrto@yandex.ua>
2015-09-22 14:16:55 +0300

    RPCT-476, RPCT-421, RPCT-479, RPCT-431, RPCT-434, DbTest: get_checked_out_files, set_signal_workcopy, undo_signal_changes tests has been added.

--- Test/u7databaseTests/FileTests.h
+++ Test/u7databaseTests/FileTests.h
@@ -21,10 +21,8 @@ private slots:
 	void is_any_checked_outTest();
 	void filesAddTest_data();
 	void filesAddTest();
-	void is_file_checkedoutTest_data();
-	void is_file_checkedoutTest();
-	void file_has_childrenTest_data();
-	void file_has_childrenTest();
+	void is_file_checkedout();
+	void file_has_children();
 	void delete_fileTest_data();
 	void delete_fileTest();
 	void check_inTest();
@@ -34,8 +32,8 @@ private slots:
 	void get_file_historyTest();
 	void get_file_stateTest();
 	void get_last_changesetTest();
-	void get_file_IdIntegetTextTest();
-	void get_file_IdIntegetrIntegerTextTest();
+	void get_file_IdIntegerTextTest();
+	void get_file_IdIntegerIntegerTextTest();
 	void get_file_infoTest();
 	void get_latest_file_versionTest();
 	void get_file_listTest();
@@ -44,13 +42,11 @@ private slots:
 	void add_or_update_fileTest();
 	void get_specific_copyTest();
 	void add_deviceTest();
+	void get_checked_out_filesTest();
 
 public:
-	static bool fileExists(int fileID);
 	static QString filesExist(QString fileID);
-	static bool add_file(int userId, QString fileName, int parentId, QString fileData);
-	static bool is_file_checkedout(int fileId);
-	static int file_has_children(int user_id, int fileId);
+	static bool add_file(int userId, QString fileName, int parentId, QString fileData, QString details);
 	static bool delete_file(int userId, int fileId);
 
 	int m_firstUserForTest = -1;
