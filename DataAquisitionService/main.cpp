844cf24cd659c945f246f5df3787ae319e20e592
Yuriy Beliy <whiteman.kr@gmail.com>
2016-04-14 11:42:08 +0300

    RPCT-720: Writing to same build catalog ability has beed added

--- DataAquisitionService/main.cpp
+++ DataAquisitionService/main.cpp
@@ -1,16 +1,16 @@
 #include <QCoreApplication>
 #include "DataAquisitionService.h"
 
-//#if defined(Q_OS_WIN) && defined(_MSC_VER)
-//	#include <vld.h>		// Enable Visula Leak Detector
-//	// vld.h includes windows.h wich redefine min/max stl functions
-//	#ifdef min
-//		#undef min
-//	#endif
-//	#ifdef max
-//		#undef max
-//	#endif
-//#endif
+#if defined(Q_OS_WIN) && defined(_MSC_VER)
+	#include <vld.h>		// Enable Visula Leak Detector
+	// vld.h includes windows.h wich redefine min/max stl functions
+	#ifdef min
+		#undef min
+	#endif
+	#ifdef max
+		#undef max
+	#endif
+#endif
 
 
 int main(int argc, char *argv[])
