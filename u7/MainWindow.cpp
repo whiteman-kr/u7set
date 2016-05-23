c16622f6688299a67de484a8782c66e6c02dab68
Serhiy Malokhatko <malokhatko@yahoo.com>
2016-05-23 14:46:08 +0300

    RPCT-882, ide: Remove Module Configuration menu item

--- u7/MainWindow.cpp
+++ u7/MainWindow.cpp
@@ -189,11 +189,6 @@ void MainWindow::createActions()
 	m_settingsAction->setEnabled(true);
 	connect(m_settingsAction, &QAction::triggered, this, &MainWindow::showSettings);
 
-	m_configuratorAction = new QAction(tr("Module Configurator..."), this);
-	m_configuratorAction->setStatusTip(tr("Run module configurator"));
-	//m_pConfiguratorAction->setEnabled(true);
-	connect(m_configuratorAction, &QAction::triggered, this, &MainWindow::runConfigurator);
-
 	m_afblEditorAction = new QAction(tr("AFBL Editor..."), this);
 	m_afblEditorAction->setStatusTip(tr("Run AFBL Editor"));
 	m_afblEditorAction->setEnabled(false);
@@ -267,7 +262,6 @@ void MainWindow::createMenus()
 	//
 	QMenu* pToolsMenu = menuBar()->addMenu(tr("&Tools"));
 
-	pToolsMenu->addAction(m_configuratorAction);
 	pToolsMenu->addAction(m_afblEditorAction);
 	pToolsMenu->addAction(m_subsystemListEditorAction);
     pToolsMenu->addAction(m_connectionsEditorAction);
