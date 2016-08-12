f3bf740d9b80826b95af01a714976eca97fe4e11
Tkachuk Roman <katranrto@yandex.ua>
2016-08-12 14:16:01 +0300

    RPCT-791: Updated u7settest project according to database changes

--- Test/u7databaseTests/main.cpp
+++ Test/u7databaseTests/main.cpp
@@ -19,7 +19,7 @@
 #include "DbControllerVersionControlTests.h"
 #include "../../lib/DbController.h"
 
-const int DatabaseProjectVersion = 83;
+const int DatabaseProjectVersion = 86;
 
 const char* DatabaseHost = "127.0.0.1";
 const char* DatabaseUser = "u7";
