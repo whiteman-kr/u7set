1fbee2df07e574c00ebdc4750656ac37f5bd741a
Yuriy Beliy <whiteman.kr@gmail.com>
2017-04-03 16:30:47 +0300

    Added file del-version-h.bat

---
+++ del-version-h.bat
@@ -0,0 +1,7 @@
+del .\BaseService\version.h
+del .\AppDataService\version.h
+del .\ConfigurationService\version.h
+del .\Metrology\version.h
+del .\TuningClient\version.h
+del .\TuningService\version.h
+del .\u7\version.h
