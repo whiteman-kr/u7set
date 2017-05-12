b814342f4fa08b0acb1d658873f46a88485278eb
Boris Vintenko <boris_vin@ukr.net>
2017-05-12 17:29:57 +0300

    RPCT-1592, TuningClient, Monitor: added information about column and line when error occurs during parsing configuration file

--- TuningClient/ConfigController.cpp
+++ TuningClient/ConfigController.cpp
@@ -195,11 +195,15 @@ void ConfigController::slot_configurationReady(const QByteArray configurationXml
 	QDomDocument xml;
 	QString parsingError;
 
-	bool result = xml.setContent(configurationXmlData, false, &parsingError);
+	int errorLine = 0;
+	int errorColumn = 0;
+
+	bool result = xml.setContent(configurationXmlData, false, &parsingError, &errorLine, &errorColumn);
 
 	if (result == false)
 	{
-		readSettings.errorMessage += parsingError + "\n";
+		QString errorStr = tr("%1, line %2, column %3").arg(parsingError).arg(errorLine).arg(errorColumn);
+		readSettings.errorMessage += errorStr + "\n";
 	}
 	else
 	{
