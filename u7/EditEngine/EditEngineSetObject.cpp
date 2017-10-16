0143814c8b330f1fc7c69dea3d78a356b8bacc1b
Serhiy Malokhatko <malokhatko@yahoo.com>
2017-10-16 15:59:51 +0300

    RPCT-1833, ide: Add EditEngine SetObject command, to save/restore whole object

---
+++ u7/EditEngine/EditEngineSetObject.cpp
@@ -0,0 +1,69 @@
+#include "EditEngineSetObject.h"
+#include "EditSchemaWidget.h"
+#include "../../VFrame30/SchemaItemAfb.h"
+#include "../../VFrame30/SchemaItemBus.h"
+#include <QMessageBox>
+
+namespace EditEngine
+{
+
+	SetObjectCommand::SetObjectCommand(EditSchemaView* schemaView,
+									   const QByteArray& oldState,
+									   const QByteArray& newState,
+									   const std::vector<std::shared_ptr<VFrame30::SchemaItem>>& items,
+									   QScrollBar* hScrollBar,
+									   QScrollBar* vScrollBar) :
+		EditCommand(schemaView, hScrollBar, vScrollBar)
+	{
+		assert(oldState.isEmpty() == false);
+		assert(newState.isEmpty() == false);
+		assert(items.empty() == false);
+
+		m_schema = schemaView->schema();
+		assert(m_schema != nullptr);
+
+		for (auto& item : items)
+		{
+			assert(item);
+
+			Record r = {oldState, newState, item};
+
+			m_items.push_back(r);
+		}
+
+		return;
+	}
+
+	void SetObjectCommand::executeCommand(EditSchemaView* schemaView)
+	{
+		std::list<std::shared_ptr<VFrame30::SchemaItem>> selection;
+
+		for (const Record& r : m_items)
+		{
+			selection.push_back(r.item);
+
+			bool ok = r.item->Load(r.newState);
+			assert(ok);
+		}
+
+		schemaView->setSelectedItems(selection);
+		return;
+	}
+
+	void SetObjectCommand::unExecuteCommand(EditSchemaView* schemaView)
+	{
+		std::list<std::shared_ptr<VFrame30::SchemaItem>> selection;
+
+		for (const Record& r : m_items)
+		{
+			selection.push_back(r.item);
+
+			bool ok = r.item->Load(r.oldState);
+			assert(ok);
+		}
+
+		schemaView->setSelectedItems(selection);
+		return;
+	}
+
+}
