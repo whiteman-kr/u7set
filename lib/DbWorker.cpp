0d3f4e41383cedb8df4087d96999f1e38905d319
Boris Vintenko <boris_vin@ukr.net>
2017-11-22 09:59:17 +0200

    RPCT-1896, u7: Removed TUNING signal from all io modules, RUN(SAFE) from AIM, DIM, OCM

--- lib/DbWorker.cpp
+++ lib/DbWorker.cpp
@@ -198,6 +198,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0180.sql", "Upgrade to version 180, Fixing DataUid generating in OCMN module"},
 	{":/DatabaseUpgrade/Upgrade0181.sql", "Upgrade to version 181, TxDiagDataSize in LMs has been changed from 176 to 188 words"},
 	{":/DatabaseUpgrade/Upgrade0182.sql", "Upgrade to version 182, update LM Scripts, LmDescriptions is now written to LM"},
+	{":/DatabaseUpgrade/Upgrade0183.sql", "Upgrade to version 183, Removed some signals from Platform Interface Controller in in-out modules and OCM/OCMN"},
 };
 
 
