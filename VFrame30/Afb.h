6539208561db0d85e53beeb1b24bf36f9907f825
Serhiy Malokhatko <malokhatko@yahoo.com>
2017-12-28 13:06:58 +0200

    RPCT-1904, sim: Making AFBComponent available from JS

--- VFrame30/Afb.h
+++ VFrame30/Afb.h
@@ -55,10 +55,17 @@ namespace Afb
 		AfbComponentPinType m_type = AfbComponentPinType::Param;
 	};
 
-	class VFRAME30LIBSHARED_EXPORT AfbComponent
+	class VFRAME30LIBSHARED_EXPORT AfbComponent : public QObject
 	{
+		Q_OBJECT
+
+		Q_PROPERTY(QString OpCode READ opCode)
+		Q_PROPERTY(QString Caption READ caption)
+		Q_PROPERTY(QString MaxInstCount READ maxInstCount)
+
 	public:
 		AfbComponent();
+		AfbComponent(const AfbComponent& that);
 
 		// Serialization
 		//
@@ -84,7 +91,12 @@ namespace Afb
 		int maxInstCount() const;
 		void setMaxInstCount(int value);
 
+		Q_INVOKABLE bool pinExists(int pinOpIndex) const;
+		Q_INVOKABLE QString pinCaption(int pinOpIndex) const;
+
 	private:
+		// Operator= is present, don't forget to add new fields to it
+		//
 		int m_opCode = -1;
 		QString m_caption;
 		int m_impVersion = -1;
@@ -92,6 +104,8 @@ namespace Afb
 		int m_maxInstCount = 0;
 
 		std::map<int, AfbComponentPin> m_pins;		// Key is OpIndex of pin - AfbComponentPin::opIndex()
+		// Operator= is present, don't forget to add new fields to it
+		//
 	};
 
 
