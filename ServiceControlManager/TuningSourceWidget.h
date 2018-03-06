03a3e26d945ca4e3aa711f924948cc95b4b49276
Vitaliy Sapronenko <vsapronenko@gmail.com>
2018-03-06 18:37:34 +0200

    RPCT-1996, ServiceControlManager: TuningSourceWidget: TuningSourceInfo data has been added

--- ServiceControlManager/TuningSourceWidget.h
+++ ServiceControlManager/TuningSourceWidget.h
@@ -6,16 +6,19 @@
 class QTableView;
 class QStandardItemModel;
 class TcpTuningServiceClient;
+class QSplitter;
 
 class TuningSourceWidget : public QWidget
 {
 	Q_OBJECT
 public:
 	explicit TuningSourceWidget(quint64 id, QString equipmentId, QWidget *parent = nullptr);
+	~TuningSourceWidget();
 
 	quint64 id() { return m_id; }
 	QString equipmentId() { return m_equipmentId; }
 signals:
+	void forgetMe();
 
 public slots:
 	void updateStateFields();
@@ -26,8 +29,17 @@ protected:
 	void closeEvent(QCloseEvent* event);
 
 private:
+	void initTable(QTableView* table, QStandardItemModel* model);
+
+private:
+	QTableView* m_infoTable = nullptr;
+	QStandardItemModel* m_infoModel = nullptr;
+
 	QTableView* m_stateTable = nullptr;
 	QStandardItemModel* m_stateModel = nullptr;
+
+	QSplitter* m_splitter = nullptr;
+
 	TcpTuningServiceClient* m_tcpClientSocket = nullptr;
 	quint64 m_id;
 	QString m_equipmentId;
