d376d816fb6bca53f7a98705e4acc0e42ea835be
Serhiy Malokhatko <malokhatko@yahoo.com>
2018-03-23 11:11:15 +0200

    RPCT-2038, build: Multithreading in schemas parsing

--- u7/Builder/Parser.h
+++ u7/Builder/Parser.h
@@ -236,6 +236,31 @@ namespace Builder
 	};
 
 
+	// Container to store schemas parse result, in multithreading
+	// After parsing all schemas call setToAppData to set result to modules
+	//
+	class ReadyParseDataContainer
+	{
+	public:
+		void add(QString equipmentId,
+				 std::shared_ptr<BushContainer> bushContainer,
+				 std::shared_ptr<VFrame30::LogicSchema> schema);
+
+		void setToAppData(AppLogicData* appData, IssueLogger* log);
+
+	private:
+		struct AppData
+		{
+			QString equipmentId;
+			std::shared_ptr<BushContainer> bushContainer;
+			std::shared_ptr<VFrame30::LogicSchema> schema;
+		};
+
+		mutable QMutex m_mutex;
+		std::list<AppData> m_appData;
+	};
+
+
 	// ------------------------------------------------------------------------
 	//
 	//		ApplicationLogicBuilder
@@ -286,10 +311,10 @@ namespace Builder
 		bool parseUfbLayer(std::shared_ptr<VFrame30::UfbSchema> ufbSchema,
 						   std::shared_ptr<VFrame30::SchemaLayer> layer);
 
-		bool parseAppLogicSchema(std::shared_ptr<VFrame30::LogicSchema> logicSchema);
+		bool parseAppLogicSchema(std::shared_ptr<VFrame30::LogicSchema> logicSchema, ReadyParseDataContainer* readyParseDataContainer, bool* interruptProcess);
 
 		bool parseAppLogicLayer(std::shared_ptr<VFrame30::LogicSchema> logicSchema,
-								std::shared_ptr<VFrame30::SchemaLayer> layer);
+								std::shared_ptr<VFrame30::SchemaLayer> layer, ReadyParseDataContainer* readyParseDataContainer);
 
 		bool multichannelProcessing(std::shared_ptr<VFrame30::LogicSchema> schema,
 									std::shared_ptr<VFrame30::SchemaLayer> layer,
