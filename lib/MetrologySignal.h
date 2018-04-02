3df60d1c9d434b021a3d0ef0d0b6755824ecef3e
Sergey Galitskiy <Galytskyi@gmail.com>
2018-04-02 15:48:19 +0300

    Filename "MetrologySignals.xml" moved to CfgFiles.h

--- lib/MetrologySignal.h
+++ lib/MetrologySignal.h
@@ -4,12 +4,12 @@
 #include "../lib/AppSignal.h"
 #include "../lib/DeviceObject.h"
 #include "../lib/XmlHelper.h"
+#include "../u7/Builder/CfgFiles.h"
 
 // Attention !!!
 // If you want to change any function writeToXml you must change CFG_FILE_VER_METROLOGY_SIGNALS
 // and write log history about changing
 
-const char* const	CFG_FILE_NAME_METROLOGY_SIGNALS	= "MetrologySignals.xml";
 const int			CFG_FILE_VER_METROLOGY_SIGNALS	= 2;
 
 // Historty of version
