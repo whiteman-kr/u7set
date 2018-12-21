3d08a3ef989eafc1ad9c6e8bce04f0b29f85cf1f
Boris Vintenko <boris_vin@ukr.net>
2018-12-21 08:51:54 +0200

    RPCT-2193, u7: Added modules for 4 phase: AIM-4PH, AOM-4PH, LM-SF00-4PH. TIM,RIM,WAIM were renamed

--- lib/DbWorker.cpp
+++ lib/DbWorker.cpp
@@ -253,6 +253,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0233.sql", "Upgrade to version 233, ArchService properties were removed from Monitor preset"},
 	{":/DatabaseUpgrade/Upgrade0234.sql", "Upgrade to version 234, Changed LM1_SR03 ID and DescriptionNumber"},
 	{":/DatabaseUpgrade/Upgrade0235.sql", "Upgrade to version 235, Added Blink signal to LM1_SR03 preset"},
+	{":/DatabaseUpgrade/Upgrade0236.sql", "Upgrade to version 236, Added LM1_SF00_4PH, AIM_4PH, AOM_4PH, and some corrections in WAIM, RIM, TIM"},
 };
 
 
