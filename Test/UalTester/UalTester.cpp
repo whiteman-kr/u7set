5a49243b850029e2dcfad3cf95895ed81a9d0e50
Sergey Galitskiy <Galytskyi@gmail.com>
2019-01-25 17:02:33 +0200

    RPCT-2217, UalTester: Added optional parameters for command line

--- Test/UalTester/UalTester.cpp
+++ Test/UalTester/UalTester.cpp
@@ -15,50 +15,74 @@ const char* const UalTester::SETTING_CFG_SERVICE_IP2 = "CfgServiceIP2";
 const char* const UalTester::SETTING_EQUIPMENT_ID = "EquipmentID";
 const char* const UalTester::SETTING_TEST_FILE_NAME  = "TestFileName";
 
-// -------------------------------------------------------------------------------------------------------------------
+const char* const UalTester::SETTING_ERROR_IGNORE = "ErrorIngnore";
+const char* const UalTester::SETTING_TEST_ID = "TestID";
+const char* const UalTester::SETTING_FROM_TEST_ID = "FromTestID";
+const char* const UalTester::SETTING_TRACE = "Trace";
+const char* const UalTester::SETTING_REPORT = "ReportFileName";
+const char* const UalTester::SETTING_PRESET_LM = "TestFileName";
 
 UalTester::UalTester(int& argc, char** argv)
 {
 	getCmdLineParams(argc, argv);
 }
 
-// -------------------------------------------------------------------------------------------------------------------
-
 UalTester::~UalTester()
 {
 }
-
-// -------------------------------------------------------------------------------------------------------------------
 
 void UalTester::getCmdLineParams(int& argc, char** argv)
 {
 	CommandLineParser cmdLineParser(argc, argv);
 
+	// main settings
+	//
 	cmdLineParser.addSingleValueOption("cfgip1", SETTING_CFG_SERVICE_IP1, "IP-addres of first Configuration Service.", "IPv4");
 	cmdLineParser.addSingleValueOption("cfgip2", SETTING_CFG_SERVICE_IP2, "IP-addres of second Configuration Service.", "IPv4");
 	cmdLineParser.addSingleValueOption("id", SETTING_EQUIPMENT_ID, "EquipmentID.", "EQUIPMENT_ID");
 	cmdLineParser.addSingleValueOption("f", SETTING_TEST_FILE_NAME, "Test file name", "TestFileName");
 
+	// optional settings
+	//
+	cmdLineParser.addSingleValueOption("errignore", SETTING_ERROR_IGNORE, "Stop testing if errors are found.", "No");
+	cmdLineParser.addSingleValueOption("test", SETTING_TEST_ID, "Run a specific test", "TEST_ID");
+	cmdLineParser.addSingleValueOption("from", SETTING_FROM_TEST_ID, "Starting from the specified test", "TEST_ID");
+	cmdLineParser.addSingleValueOption("trace", SETTING_TRACE, "Line test execution report", "No");
+	cmdLineParser.addSingleValueOption("report", SETTING_REPORT, "Report file name", "ReportFileName");
+	cmdLineParser.addSingleValueOption("lm", SETTING_PRESET_LM, "Run only tests compatible with the specified LM preset", "LM_ID");
+
 	cmdLineParser.parse();
 
-	m_cfgServiceIP1Str = cmdLineParser.settingValue(SETTING_CFG_SERVICE_IP1);
-	m_cfgServiceIP2Str = cmdLineParser.settingValue(SETTING_CFG_SERVICE_IP2);
+	// main settings
+	//
+	m_cfgServiceIP1 = cmdLineParser.settingValue(SETTING_CFG_SERVICE_IP1);
+	m_cfgServiceIP2 = cmdLineParser.settingValue(SETTING_CFG_SERVICE_IP2);
 	m_equipmentID = cmdLineParser.settingValue(SETTING_EQUIPMENT_ID);
 	m_testFileName = cmdLineParser.settingValue(SETTING_TEST_FILE_NAME);
-}
 
-// -------------------------------------------------------------------------------------------------------------------
+	// optional settings
+	//
+	m_errorIngnoreStr = cmdLineParser.settingValue(SETTING_ERROR_IGNORE);
+	m_testID = cmdLineParser.settingValue(SETTING_TEST_ID);
+	m_fromTestID = cmdLineParser.settingValue(SETTING_FROM_TEST_ID);
+	m_traceStr = cmdLineParser.settingValue(SETTING_TRACE);
+	m_reportFileName = cmdLineParser.settingValue(SETTING_REPORT);
+	m_presetLM = cmdLineParser.settingValue(SETTING_PRESET_LM);
+}
 
 bool UalTester::cmdLineParamsIsValid()
 {
-	if (m_cfgServiceIP1Str.isEmpty() == true && m_cfgServiceIP2Str.isEmpty() == true )
+	// main settings
+	//
+
+	if (m_cfgServiceIP1.isEmpty() == true && m_cfgServiceIP2.isEmpty() == true )
 	{
 		qDebug() << "IP-addres of Configuration Service is empty";
 		return false;
 	}
 
-	m_cfgSocketAddress1.setAddressPortStr(m_cfgServiceIP1Str, PORT_CONFIGURATION_SERVICE_CLIENT_REQUEST);
-	m_cfgSocketAddress2.setAddressPortStr(m_cfgServiceIP2Str, PORT_CONFIGURATION_SERVICE_CLIENT_REQUEST);
+	m_cfgSocketAddress1.setAddressPortStr(m_cfgServiceIP1, PORT_CONFIGURATION_SERVICE_CLIENT_REQUEST);
+	m_cfgSocketAddress2.setAddressPortStr(m_cfgServiceIP2, PORT_CONFIGURATION_SERVICE_CLIENT_REQUEST);
 
 	if (m_cfgSocketAddress1.isValidIPv4(m_cfgSocketAddress1.addressStr()) == false)
 	{
@@ -80,14 +104,61 @@ bool UalTester::cmdLineParamsIsValid()
 
 	if (m_testFileName.isEmpty() == true)
 	{
-		qDebug() << "Test file name is epmpty";
+		qDebug() << "Test file name is empty";
 		return false;
+	}
+
+	// optional settings
+	//
+
+	if (m_errorIngnoreStr.isEmpty() == false)
+	{
+		bool paramOk = false;
+
+		if (m_errorIngnoreStr.compare("yes", Qt::CaseInsensitive) == 0)
+		{
+			paramOk = true;
+			m_errorIngnore = true;
+		}
+
+		if (m_errorIngnoreStr.compare("no", Qt::CaseInsensitive) == 0)
+		{
+			paramOk = true;
+			m_errorIngnore = false;
+		}
+
+		if (paramOk == false)
+		{
+			qDebug() << "Invalid value of the parameter \"errignore\", specify \"yes\" or \"no\"";
+			return false;
+		}
+	}
+
+	if (m_traceStr.isEmpty() == false)
+	{
+		bool paramOk = false;
+
+		if (m_traceStr.compare("yes", Qt::CaseInsensitive) == 0)
+		{
+			paramOk = true;
+			m_trace = true;
+		}
+
+		if (m_traceStr.compare("no", Qt::CaseInsensitive) == 0)
+		{
+			paramOk = true;
+			m_trace = false;
+		}
+
+		if (paramOk == false)
+		{
+			qDebug() << "Invalid value of the parameter \"trace\", specify \"yes\" or \"no\"";
+			return false;
+		}
 	}
 
 	return true;
 }
-
-// -------------------------------------------------------------------------------------------------------------------
 
 bool UalTester::start()
 {
@@ -124,5 +195,3 @@ bool UalTester::start()
 
 	return true;
 }
-
-// -------------------------------------------------------------------------------------------------------------------
--- Test/UalTester/UalTester.h
+++ Test/UalTester/UalTester.h
@@ -21,15 +21,32 @@ protected:
 	static const char* const SETTING_EQUIPMENT_ID;
 	static const char* const SETTING_TEST_FILE_NAME;
 
+	static const char* const SETTING_ERROR_IGNORE;
+	static const char* const SETTING_TEST_ID;
+	static const char* const SETTING_FROM_TEST_ID;
+	static const char* const SETTING_TRACE;
+	static const char* const SETTING_REPORT;
+	static const char* const SETTING_PRESET_LM;
+
 private:
 
-	QString			m_cfgServiceIP1Str;
-	QString			m_cfgServiceIP2Str;
+	QString			m_cfgServiceIP1;
+	QString			m_cfgServiceIP2;
 	QString			m_equipmentID;
 	QString			m_testFileName;
 
+	QString			m_errorIngnoreStr;
+	QString			m_testID;
+	QString			m_fromTestID;
+	QString			m_traceStr;
+	QString			m_reportFileName;
+	QString			m_presetLM;
+
 	HostAddressPort m_cfgSocketAddress1;
 	HostAddressPort m_cfgSocketAddress2;
+
+	bool			m_errorIngnore = true;
+	bool			m_trace = false;
 
 	void			getCmdLineParams(int& argc, char** argv);
 	bool			cmdLineParamsIsValid();
