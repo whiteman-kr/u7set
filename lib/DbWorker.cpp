6d8ef8b7504f7bc34b06c996c908c423b8fba9ff
Vitaliy Sapronenko <vsapronenko@gmail.com>
2019-01-28 17:36:40 +0200

    RPCT-2226: Property unit have been made editable for output analog signals

--- lib/DbWorker.cpp
+++ lib/DbWorker.cpp
@@ -262,6 +262,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0242.sql", "Upgrade to version 242, AOM-4PH Preset corrections"},
 	{":/DatabaseUpgrade/Upgrade0243.sql", "Upgrade to version 243, AIM-4PH Preset corrections"},
 	{":/DatabaseUpgrade/Upgrade0244.sql", "Upgrade to version 244, Blink signal was added to LM1-SR04, LM1-SF00-4PH Presets"},
+	{":/DatabaseUpgrade/Upgrade0245.sql", "Upgrade to version 245, Unit have been made editable for output analog signals"},
 };
 
 
