8f0823d3cadb2451782882d06be53bc9901f508e
Sergey Galitskiy <Galytskyi@gmail.com>
2019-02-06 18:23:21 +0200

    RPCT-2242: Added UnitsConvertor class to convert values from ElectricLimics to PhysicalLimits (Only for block AIM)

---
+++ lib/UnitsConvertor.h
@@ -0,0 +1,28 @@
+﻿#ifndef UNITSCONVERTOR_H
+#define UNITSCONVERTOR_H
+
+#include <assert.h>
+
+#include "Types.h"
+
+
+const double RESISTOR_V_0_5 = 0.25;
+
+
+class UnitsConvertor : public QObject
+{
+	Q_OBJECT
+
+public:
+
+	explicit UnitsConvertor(QObject *parent = nullptr);
+	virtual ~UnitsConvertor();
+
+public:
+
+	Q_INVOKABLE QVariant physicalToElectric(double val, double electricLowLimit, double electricHighLimit, int unitID, int sensorType);
+	Q_INVOKABLE QVariant electricToPhysical(double val, double electricLowLimit, double electricHighLimit, int unitID, int sensorType);
+};
+
+
+#endif // UNITSCONVERTOR_H
