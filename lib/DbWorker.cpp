c511a9171252dbed2f5f40c5112c8af31d159a2b
Boris Vintenko <boris_vin@ukr.net>
2019-02-11 15:16:52 +0200

    RPCT-2249, u7: SchemaTags property is added to Monitor and TuningClient presets

--- lib/DbWorker.cpp
+++ lib/DbWorker.cpp
@@ -273,6 +273,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0253.sql", "Upgrade to version 253, ImpVersion and MaxInstCount made up to date in AFB elements descriptions"},
 	{":/DatabaseUpgrade/Upgrade0254.sql", "Upgrade to version 254, PhysicalLimits were removed WAIM-4PH, they are calculated from ElectricUnits"},
 	{":/DatabaseUpgrade/Upgrade0255.sql", "Upgrade to version 255, PhysicalLimits calculation error messages are processed in WAIM and AIM"},
+	{":/DatabaseUpgrade/Upgrade0256.sql", "Upgrade to version 256, SchemaTags property was added to Monitor and TuningClient presets"},
 };
 
 
