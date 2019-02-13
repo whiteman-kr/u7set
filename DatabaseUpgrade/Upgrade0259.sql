563fa7f56789fe1cdda94c92c3509472ca2b08b5
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2019-02-13 15:22:13 +0200

    RPCT-2251, db: undo_changes, disabled all triggers on delete row from FileInstance. God keeps us safe.

--- DatabaseUpgrade/Upgrade0259.sql
+++ DatabaseUpgrade/Upgrade0259.sql
@@ -7,120 +7,140 @@ CREATE INDEX file_index_checkedoutinstanceid ON file USING btree (checkedininsta
 
 -- check_in, data comparision is changed to compare saved before md5
 --
-CREATE OR REPLACE FUNCTION check_in(
+-- Function: public.undo_changes(integer, integer[])
+--
+CREATE OR REPLACE FUNCTION undo_changes(
     user_id integer,
-    file_ids integer[],
-    checkin_comment text)
+    file_ids integer[])
   RETURNS SETOF objectstate AS
 $BODY$
 DECLARE
-	NewChangesetID int;
 	WasCheckedOut int;
 	file_id int;
 	file_user int;
 	is_user_admin boolean;
+	deleted_count int;
+	checked_out_instance_id uuid;
 	file_result ObjectState;
-	max_file_changeset_id int;
-	file_action int;	
-	changed_file_ids integer[];
-	undo_file_ids integer[];
-	last_file_data_md5 text;
-	curr_file_data_md5 text;
-	was_moved_or_renamed integer;
+	moved_from_parent_id int;
+	before_move_file_id int;
+	renamed_from text;		-- file was renamed in this check out, then it has initail file name
 BEGIN
-	-- Check if files really checked out
-	is_user_admin :=  is_admin(user_id);
 
+	is_user_admin := is_admin(user_id);
+
+	-- sort file_ids in descending order, so children will be deleted (if it was not checked in yet) first
+	--
+	file_ids := uniq(sort_desc(file_ids));
+
+	-- undo operation for each file
+	--
 	FOREACH file_id IN ARRAY file_ids
 	LOOP
 		-- Check if files really checked out
+		--
 		SELECT count(*) INTO WasCheckedOut FROM CheckOut WHERE FileID = file_id;
 		IF (WasCheckedOut <> 1)	THEN
-			RAISE 'File is not checked out: %', file_id;
+			-- RAISE 'File is not checked out or file is not exists: %', file_id;
+			file_result := get_file_state(file_id);
+			RETURN NEXT file_result;
+			CONTINUE;
 		END IF;
 
-		-- Check if the file can be checked_in by this user_id
-		file_user := (SELECT UserId FROM CheckOut WHERE FileID = file_id);
+		-- Check if the file can be undo by this user_id
+		SELECT UserId INTO file_user FROM CheckOut WHERE FileID = file_id;
 
 		IF (is_user_admin = FALSE AND file_user <> user_id) THEN
 			RAISE 'User % has no right to perform operation.', user_id;
 		END IF;
 
-		-- Check if the file was really changed
-        max_file_changeset_id := (SELECT max(ChangesetID) FROM FileInstance WHERE FileID = file_id);
-        file_action := (SELECT Action FROM FileInstance WHERE FileID = file_id AND ChangesetID IS NULL);
+		-- During removing record from File (if it has not been checked_in before),
+		-- if there is any dependants exception can occure
+		--
+		BEGIN
+            -- set moved_from_parent_id, before_move_file_id, renamed_from
+            --
+            SELECT MovedFromParentID, BeforeMoveFileID, RenamedFrom
+                INTO moved_from_parent_id, before_move_file_id, renamed_from
+                FROM FileInstance 
+                WHERE FileID = file_id AND ChangesetID IS NULL;
+		
+			-- update table file, set CheckedOutIntsnceID to NULL
+			--
+			checked_out_instance_id := (SELECT CheckedOutInstanceID FROM File WHERE FileID = file_id);
+			
+			UPDATE File SET CheckedOutInstanceID = NULL WHERE FileID = file_id;
 
-        IF (max_file_changeset_id IS NULL OR
-            file_action <> 2)   -- 2 is Modified
-        THEN
-            -- File has not been checked in yet
-            changed_file_ids := array_append(changed_file_ids,  file_id);
-        ELSE
-            -- Get last check in data
-            last_file_data_md5 := (SELECT md5 FROM FileInstance WHERE FileID = file_id AND ChangesetID = max_file_changeset_id);
+			-- Delete from file instance all these files
+			--
+			ALTER TABLE FileInstance DISABLE TRIGGER ALL;
+				DELETE FROM FileInstance WHERE FileInstanceID = checked_out_instance_id;
+			ALTER TABLE FileInstance ENABLE TRIGGER ALL;
 
-            -- Get work copy data
-            curr_file_data_md5 := (SELECT md5 FROM FileInstance WHERE FileID = file_id AND ChangesetID IS NULL);
+			-- Delete all check outs
+			--
+			DELETE FROM CheckOut WHERE FileID = file_id;
 
-			-- If file was moved or renamed we mast check it in
-            was_moved_or_renamed := (SELECT COUNT(*) FROM FileInstance WHERE FileID = file_id AND ChangesetID IS NULL AND (MoveText <> '' OR RenameText <> ''));
+			-- if column File.CheckedInIntsnceID is NULL then this file was not checked in, and we have to TRY to remove it from the table
+			--
+			DELETE FROM File WHERE FileID = file_id AND (CheckedInInstanceID IS NULL) RETURNING * INTO deleted_count;
 
-            -- Check if data was changed or file was moved or file was renamed
-            IF (last_file_data_md5 <> curr_file_data_md5 OR was_moved_or_renamed <> 0)
-            THEN
-                changed_file_ids := array_append(changed_file_ids,  file_id);
-            ELSE
-                undo_file_ids := array_append(undo_file_ids,  file_id);
-            END IF;
-         END IF;
+			-- form output result
+			--
+			IF (deleted_count = 0 OR deleted_count IS NULL)
+			THEN
+				file_result := get_file_state(file_id);
+			ELSE
+				file_result.id := file_id;
+				file_result.deleted := true;
+				file_result.checkedout := false;
+				file_result.action := 3;
+				file_result.userid := user_id;
+				file_result.errcode := 0;
+			END IF;
 
-	END LOOP;
+            EXCEPTION WHEN foreign_key_violation THEN
+                -- cannot remove file? mark it as deleted, mark fileinstance action as deleted
+                --
+                UPDATE FileInstance SET Action = 3 WHERE FileInstanceID = (SELECT CheckedOutInstanceID FROM File WHERE FileID = file_id);
 
-	-- Undo unchanged files
-	IF (array_length(undo_file_ids, 1) <> 0) 
-	THEN
-        PERFORM undo_changes(user_id, undo_file_ids);
-	END IF;
+                -- form output result
+                --
+                file_result := get_file_state(file_id);
+            END;
 
-	-- Check if changed_file_ids is empty
-	IF (array_length(changed_file_ids, 1) = 0 OR 
-	    array_length(changed_file_ids, 1) IS NULL)
-	THEN
-        -- return table of ObjectState;
-        FOREACH file_id IN ARRAY file_ids
-        LOOP
-            file_result := get_file_state(file_id);
-            RETURN NEXT file_result;
-        END LOOP;
-        RETURN;        
-	END IF;
+	        -- Restore moved file
+        	--
+           	IF (before_move_file_id <> -1)
+		THEN
+                -- File was moved, return old file id to tables File and FileInstance
+                -- As FileInstance is related to File via FileID, it's impossible just to change FileID in File
+                -- (1) So create a new record with old file before_move_file_id, (2) set the old parent to it
+                -- and then (3) change FileInstance.Fileid to before_move_file_id
+                -- 
 
-	-- Add new record to Changeset
-	INSERT INTO Changeset (UserID, Comment, File) VALUES (user_id, checkin_comment, TRUE) RETURNING ChangesetID INTO NewChangesetID;
+                -- Create the new record in Files table, with the new (actually old) file id (before_move_file_id)
+                --
+                INSERT INTO File (FileID, Name, Created, ParentID, Deleted, CheckedInInstanceId, CheckedOutInstanceID, Attributes) 
+                	SELECT before_move_file_id, Name, Created, moved_from_parent_id, Deleted, CheckedInInstanceId, CheckedOutInstanceID, Attributes FROM File WHERE FileID = file_id;
 
-	-- Set File.Deleted flag if action in FileInstance is Deleted
-	UPDATE File SET Deleted = TRUE
-		WHERE
-			FileID = ANY(changed_file_ids) AND
-			3 = (SELECT Action FROM FileInstance FI WHERE FI.FileInstanceID = CheckedOutInstanceID AND FI.FileID = FileID);
+                UPDATE FileInstance SET FileID = before_move_file_id WHERE FileID = file_id;
 
-	-- Set CheckedInInstance to current CheckedOutInstance, and set CheckedOutInstanceID to NULL
-	UPDATE File SET CheckedInInstanceID = CheckedOutInstanceID WHERE FileID = ANY(changed_file_ids);
-	UPDATE File SET CheckedOutInstanceID = NULL WHERE FileID = ANY(changed_file_ids);
+                -- Delete old record from File (was creted in api.move_file)
+                --
+                DELETE FROM File WHERE FileID = file_id;
+			END IF;
 
-	-- Update FileInstance, set it's ChangesetID
-	UPDATE FileInstance SET ChangesetID = NewChangesetID WHERE FileID = ANY(changed_file_ids) AND ChangesetID IS NULL;
+			-- Restore renamed file name
+			--
+			IF (renamed_from <> '') THEN
+				UPDATE File SET Name = renamed_from WHERE FileID = file_id;
+			END IF;
 
-	-- Remove CheckOut roecords
-	DELETE FROM CheckOut WHERE FileID = ANY(changed_file_ids);
-
-	-- return table of ObjectState;
-	FOREACH file_id IN ARRAY file_ids
-	LOOP
-		file_result := get_file_state(file_id);
 		RETURN NEXT file_result;
 	END LOOP;
+
 	RETURN;
-END;
+END
 $BODY$
-  LANGUAGE plpgsql;
+LANGUAGE plpgsql;
