bc94920c276d9a0540f5d77ef85a48b57235edfb
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2019-04-11 10:57:28 +0300

    RPCT-2389, u7: Set styles to TabBar header indead of setting to cental widget, makes PropertyEditor using standart fonts

--- u7/CentralWidget.h
+++ u7/CentralWidget.h
@@ -1,47 +1,7 @@
 #pragma once
 
 class MainTabPage;
-#include <QSize>
 
-class TabBar:public QTabBar
-{
-public:
-	TabBar() : QTabBar()
-	{
-		setUsesScrollButtons(false);
-	}
-
-	void setMinimumTabSizeHint(const QSize& value)
-	{
-		m_tabMinimumSize = value;
-	}
-
-protected:
-
-	virtual QSize minimumTabSizeHint(int index) const override
-	{
-		Q_UNUSED(index);
-		return m_tabMinimumSize;
-	}
-
-	virtual QSize sizeHint() const override
-	{
-		int tabCount = count();
-		if (tabCount == 0)
-		{
-			tabCount = 1;
-		}
-
-		int minimumWidth = m_tabMinimumSize.width() * tabCount;
-
-		QSize s(minimumWidth, m_tabMinimumSize.height());
-		return s;
-	}
-
-private:
-	QSize m_tabMinimumSize = QSize(100, 50);
-
-};
 
 class CentralWidget : public QTabWidget
 {
