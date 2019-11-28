22a3ca437a80468a61c18e351fff91878785656f
Boris Vintenko <boris_vin@ukr.net>
2019-11-28 12:03:19 +0200

    Update .gitlab-ci.yml

--- u7setinstall/createinstall.bat
+++ u7setinstall/createinstall.bat
@@ -15,24 +15,24 @@ md packages\u7set.metrology\data
 
 echo --------------- Copying Source Files ------------------
 
-xcopy bin\release\*.dll packages\u7set\data /sy
-copy bin\release\vcredist_x64.exe packages\u7set\data
+xcopy ..\bin\release\*.dll packages\u7set\data /sy
+copy ..\bin\release\vcredist_x64.exe packages\u7set\data
 
-xcopy bin\release\scripthelp packages\u7set.rpct\data\scripthelp /sy
-copy bin\release\u7.exe packages\u7set.rpct\data
-copy bin\release\mconf.exe packages\u7set.rpct\data
+xcopy ..\bin\release\scripthelp packages\u7set.rpct\data\scripthelp /sy
+copy ..\bin\release\u7.exe packages\u7set.rpct\data
+copy ..\bin\release\mconf.exe packages\u7set.rpct\data
 
-copy bin\release\CfgSrv.exe packages\u7set.srv\data
-copy bin\release\scm.exe packages\u7set.srv\data
+copy ..\bin\release\CfgSrv.exe packages\u7set.srv\data
+copy ..\bin\release\scm.exe packages\u7set.srv\data
 
-copy bin\release\ArchSrv.exe packages\u7set.monitor\data
-copy bin\release\AppDataSrv.exe packages\u7set.monitor\data
-copy bin\release\Monitor.exe packages\u7set.monitor\data
+copy ..\bin\release\ArchSrv.exe packages\u7set.monitor\data
+copy ..\bin\release\AppDataSrv.exe packages\u7set.monitor\data
+copy ..\bin\release\Monitor.exe packages\u7set.monitor\data
 
-copy bin\release\TuningClient.exe packages\u7set.tuning\data
-copy bin\release\TuningSrv.exe packages\u7set.tuning\data
+copy ..\bin\release\TuningClient.exe packages\u7set.tuning\data
+copy ..\bin\release\TuningSrv.exe packages\u7set.tuning\data
 
-copy bin\release\Metrology.exe packages\u7set.metrology\data
+copy ..\bin\release\Metrology.exe packages\u7set.metrology\data
 
 echo --------------- Building the installer ------------------
 
