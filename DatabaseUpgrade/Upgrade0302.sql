f680d73f3bf6bba6e6770397928550c1a0fb7a4c
Yuriy Beliy <whiteman.kr@gmail.com>
2019-12-03 14:02:02 +0200

    RPCT-2475: add_signal stored procdure has been updated.

--- DatabaseUpgrade/Upgrade0302.sql
+++ DatabaseUpgrade/Upgrade0302.sql
@@ -62,3 +62,67 @@ ALTER TABLE signalinstance
 	DROP COLUMN IF EXISTS tuninglowboundint,
 	DROP COLUMN IF EXISTS tuninghighboundint,
 	DROP COLUMN IF EXISTS archive;
+
+CREATE OR REPLACE FUNCTION public.add_signal(
+    user_id integer,
+	signal_type integer,
+	channel_count integer)
+	RETURNS SETOF objectstate
+	LANGUAGE 'plpgsql'
+
+    COST 100
+	VOLATILE
+	ROWS 1000
+AS $BODY$
+DECLARE
+    newGroupID integer;
+	channel integer;
+	newSignalID integer;
+	newSignalInstanceID integer;
+	appSignalID varchar;
+	customAppSignalID varchar;
+	os objectstate;
+BEGIN
+    IF channel_count < 1 THEN
+	    channel_count = 1;
+	END IF;
+
+    IF channel_count > 4 THEN
+	    RAISE 'Signal channelCount must be equal or less then 4';
+	END IF;
+
+    IF channel_count > 1 THEN
+	    INSERT INTO SignalGroup DEFAULT VALUES RETURNING SignalGroupID INTO newGroupID;
+	ELSE
+	    -- all single-channel signals are placed in group 0
+		newGroupID = 0;
+	END IF;
+
+    FOR channel IN 1..channel_count LOOP
+	    INSERT INTO Signal (SignalGroupID, Channel, Type, Deleted, UserID) VALUES (newGroupID, channel-1, signal_type, false, user_id) RETURNING SignalID INTO newSignalID;
+		INSERT INTO CheckOut (UserID, SignalID) VALUES (user_id, newSignalID);
+
+        appSignalID = '#SIGNAL' || newSignalID::text;
+		customAppSignalID = 'SIGNAL' || newSignalID::text;
+
+        IF channel_count > 1 THEN
+		    appSignalID = appSignalID || '_' || chr(64 + channel);
+			customAppSignalID = customAppSignalID || '_' || chr(64 + channel);
+		END IF;
+
+        INSERT INTO SignalInstance (SignalID, AppSignalID, CustomAppSignalID, Action) VALUES (newSignalID, appSignalID,  customAppSignalID, 1) RETURNING SignalInstanceID INTO newSignalInstanceID;
+
+        UPDATE Signal SET CheckedOutInstanceID = newSignalInstanceID WHERE Signal.SignalID = newSignalID;
+
+        os.ID = newSignalID;
+		os.deleted = FALSE;
+		os.checkedout = TRUE;
+		os.action = 1;
+		os.userID = user_id;
+		os.errCode = 0;
+
+        RETURN NEXT os;
+	END LOOP;
+END
+$BODY$;
+
