21796cf839461990ecc53f5e20135261e2599902
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2020-03-21 15:22:26 +0200

    RPCT-2732, ide: Change set_flgas version so it can be updated after changing OpCode to 255 from 65535. Db Version 313

--- lib/DbWorker.cpp
+++ lib/DbWorker.cpp
@@ -330,6 +330,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0310.sql", "Upgrade to version 310, LAN section is modified in LM description files"},
 	{":/DatabaseUpgrade/Upgrade0311.sql", "Upgrade to version 311, Added AdditionSizes section to AFB bus_switch in LM description files"},
 	{":/DatabaseUpgrade/Upgrade0312.sql", "Upgrade to version 312, Removed outs med_in, max_in, min_in for median AFBs, set sim functions for AFBs"},
+	{":/DatabaseUpgrade/Upgrade0313.sql", "Upgrade to version 313, Changed set_flags vesrion so it can be updated after new opcode 255"},
 };
 
 int DbWorker::counter = 0;
