0d4c287d4c90e1ea15a7318ffa2b8212b522e6b3
Sergey Galitskiy <Galytskyi@gmail.com>
2021-02-04 16:30:19 +0200

    RPCT-2991, PacketSourceConsole: Loading configuration from CfgService.

---
+++ Tools/PacketSourceConsole/Stable.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+
+// C includes, must be before c++ includes
+//
+#include <assert.h>
+#include <stdint.h>
+
+// C++ includes
+//
+#include <algorithm>
+#include <atomic>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <vector>
+#include <set>
+#include <type_traits>
+
+#ifdef _MSC_VER
+	#pragma warning(push)
+	#pragma warning(disable : 6011)
+	#pragma warning(disable : 4251)
+	#pragma warning(disable : 4127)
+	#pragma warning(disable : 6326)
+#endif
+
+// Qt includes
+//
+#include <QAbstractSocket>
+
+#include <QCommandLineParser>
+#include <QCoreApplication>
+
+#include <QDebug>
+#include <QDateTime>
+
+#include <QElapsedTimer>
+#include <QEventLoop>
+
+#include <QFile>
+#include <QFileInfo>
+
+#include <QHash>
+#include <QHostAddress>
+
+#include <QMap>
+#include <QMetaClassInfo>
+#include <QMetaEnum>
+#include <QMetaObject>
+#include <QMutex>
+#include <QMutexLocker>
+
+#include <QObject>
+
+#include <QQueue>
+
+#include <QSettings>
+#include <QString>
+#include <QStringList>
+
+#include <QTextStream>
+#include <QThread>
+#include <QTimer>
+#include <QTranslator>
+
+#include <QtCore>
+#include <QtGlobal>
+#include <QtNetwork/QHostAddress>
+
+#include <QUdpSocket>
+#include <QUuid>
+
+#include <QVariant>
+#include <QVector>
+
+#include <QXmlStreamReader>
+#include <QXmlStreamWriter>
+
+#ifdef _MSC_VER
+	#pragma warning(pop)
+#endif
+
+// Disable some warnings
+//
+#ifdef Q_OS_WIN
+#pragma warning(disable : 4482)		// nonstandard extension used: enum 'enum' used in qualified name
+#pragma warning(disable : 4251)
+
+	// Disable 4996 warning
+#ifndef _SCL_SECURE_NO_WARNINGS
+	#define _SCL_SECURE_NO_WARNINGS
+#endif
+#endif
+
+// Turn on memory leaks detection
+//
+#if defined (Q_OS_WIN) && defined (Q_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+	#ifndef DBG_NEW
+		#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+		#define new DBG_NEW
+	#endif
+#endif
+
+
