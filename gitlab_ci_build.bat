f4dab1766ba90c51172ed5459f25f4eca5e28b9b
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2021-03-30 17:27:44 +0300

    GitLab CI: Update .gitlab-ci.yml file, set shell to powershell

---
+++ gitlab_ci_build.bat
@@ -0,0 +1,1 @@
+call "C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" x86_amd64 && cl.exe && %QT_BIN_DIR%/bin/qmake --version && %QT_BIN_DIR%/bin/qmake.exe %CI_PROJECT_DIR%/u7set.pro -spec win32-msvc && C:/Qt/Tools/QtCreator/bin/jom.exe qmake_all && C:/Qt/Tools/QtCreator/bin/jom.exe
