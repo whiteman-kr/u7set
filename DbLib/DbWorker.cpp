6de3d0c7d15fb649bd8d71475034fdb683deb49f
Boris Vintenko <boris_vin@ukr.net>
2021-11-19 14:34:09 +0200

    RPCT-3199, u7: MAC addresses are checked for uniqueness on configuration, Database upgrade

--- DbLib/DbWorker.cpp
+++ DbLib/DbWorker.cpp
@@ -384,6 +384,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0362.sql", "Upgrade to version 362, Added synchronization pins to DeadZone in LM8-SR10"},
 	{":/DatabaseUpgrade/Upgrade0363.sql", "Upgrade to version 363, Using setParamIntValue instead of setAfbParamByOpName in voter AFB in all LMs"},
 	{":/DatabaseUpgrade/Upgrade0364.sql", "Upgrade to version 364, 2-Tuning LAN Configuration support, Added mV_Type_L, mV_Type_M and mV_Raw_m1200_p1200 sensor types"},
+	{":/DatabaseUpgrade/Upgrade0365.sql", "Upgrade to version 365, MAC addresses are checked for uniqueness on configuration building, unused LANs filled with zeroes"},
 };
 
 int DbWorker::counter = 0;
