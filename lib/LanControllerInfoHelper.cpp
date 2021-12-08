44690c5f9987c9cd2528b5042c2abab6b315ae0e
Yuriy Beliy <whiteman.kr@gmail.com>
2021-12-08 15:47:31 +0200

    RPCT-3120: Errors handling improvement.

--- lib/LanControllerInfoHelper.cpp
+++ lib/LanControllerInfoHelper.cpp
@@ -83,6 +83,17 @@ bool LanControllerInfoHelper::getInfo(const Hardware::DeviceModule& lm,
 			{
 				const Hardware::DeviceObject* tunService = equipmentSet.deviceObject(lanControllerInfo->tuningServiceID).get();
 
+				if (tunService == nullptr)
+				{
+					// Property %1.%2 is linked to undefined software ID %3.
+					//
+					log->errCFG3021(deviceController->equipmentIdTemplate(),
+									EquipmentPropNames::TUNING_SERVICE_ID,
+									lanControllerInfo->tuningServiceID);
+
+					return false;
+				}
+
 				if (tunService->isController() == false)
 				{
 					QStringList controllersIDs;
