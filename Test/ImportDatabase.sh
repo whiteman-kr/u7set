769e10d6a686e04f1759a63808a6dae91fe9d370
Boris Vintenko <boris_vin@ukr.net>
2023-01-12 17:11:02 +0200

    RPCT-3311, u7set: working on linux testing on docker containers

---
+++ Test/ImportDatabase.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+      echo "Database name is empty! Syntax: ImportDatabase.sh database_name_without_dot_sql"
+      exit 1
+fi
+
+if [[ -f "$1.sql" ]]; then
+    echo "File $1.sql exists."
+else
+    echo "File $1.sql does not exist!"
+    exit 1
+fi
+
+echo Trying to create database $1
+
+if psql -lqt | cut -d \| -f 1 | grep -qw $1; then
+    echo Database $1 Already exists!
+    # $? is 0
+else
+    psql -U postgres -c "CREATE DATABASE $1 OWNER u7;"
+    psql $1 < $1.sql
+    # $? is 1
+fi
