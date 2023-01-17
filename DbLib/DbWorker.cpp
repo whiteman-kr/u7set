a3d1f59d199604229d69ede467881f4a2c274ca6
Boris Vintenko <boris_vin@ukr.net>
2023-01-17 16:39:22 +0200

    RPCT-3357, u7: Add SSL Value to property SecurityLevel to all services

--- DbLib/DbWorker.cpp
+++ DbLib/DbWorker.cpp
@@ -398,6 +398,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0376.sql", "Upgrade to version 376, Shortened some math_afbs captions for LMs SR05, SR20"},
 	{":/DatabaseUpgrade/Upgrade0377.sql", "Upgrade to version 377, Added CheckHostname to ConfigService preset"},
 	{":/DatabaseUpgrade/Upgrade0378.sql", "Upgrade to version 378, Add property SecurityLevel to all software services"},
+	{":/DatabaseUpgrade/Upgrade0379.sql", "Upgrade to version 379, Add SSL to property SecurityLevel to all software services"},
 };
 
 int DbWorker::counter = 0;
