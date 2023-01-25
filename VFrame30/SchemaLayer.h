821cf821d2d31b5939612d984a5467bcaa92bd14
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2023-01-25 11:32:42 +0200

    RPCT-3246, VFrame30: Resolved merge issues

--- VFrame30/SchemaLayer.h
+++ VFrame30/SchemaLayer.h
@@ -3,13 +3,12 @@
 #include "../CommonLib/Factory.h"
 #include "../CommonLib/DebugInstCounter.h"
 #include "../Proto/ProtoSerialization.h"
+#include "SchemaItem.h"
 #include "SchemaPoint.h"
 
 namespace VFrame30
 {
 	class SchemaLayer;
-	class SchemaItem;
-
 	using SchemaLayerPtr = std::shared_ptr<VFrame30::SchemaLayer>;
 	class SchemaLayer;
 	class Schema;
