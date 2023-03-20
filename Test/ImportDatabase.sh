cd914c28ff0b9c32353bb0216b3beabe0885789d
Boris Vintenko <boris_vin@ukr.net>
2023-03-20 17:07:23 +0200

    RPCT-3311, u7set: WSL databases are imported from local filesystem

--- Test/ImportDatabase.sh
+++ Test/ImportDatabase.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-if [ -z "$1" ]
+if [ -z "$1" ] || [ -z "$2" ]
 then
-      echo "Database name is empty! Syntax: ImportDatabase.sh database_name_without_dot_sql"
+      echo "Database name is empty! Syntax: ImportDatabase.sh dababase_file.sql database_name"
       exit 1
 fi
 
-if [[ -f "$1.sql" ]]; then
-    echo "File $1.sql exists."
+if [[ -f "$1" ]]; then
+    echo "File $1 exists."
 else
-    echo "File $1.sql does not exist!"
+    echo "File $1 does not exist!"
     exit 1
 fi
 
-if psql -lqt | cut -d \| -f 1 | grep -qw $1; then
-    echo Database $1 already exists, deleting it.
-    psql -U postgres -c "DROP DATABASE $1;"
+if psql -lqt | cut -d \| -f 1 | grep -qw $2; then
+    echo Database $2 already exists, deleting it.
+    psql -U postgres -c "DROP DATABASE $2;"
 fi
 
-echo Importing database $1.
+echo Importing database $2.
 
-date
-
-psql -U postgres -c "CREATE DATABASE $1 OWNER u7;"
-psql $1 < $1.sql
-
-date
+psql -U postgres -c "CREATE DATABASE $2 OWNER u7;"
+time psql $2 < $1
