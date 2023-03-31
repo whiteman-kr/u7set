a05e4e2c42694027a31cc0008272a60ab2cf2455
Boris Vintenko <boris_vin@ukr.net>
2023-03-31 11:47:55 +0300

    RPCT-3460, Tests: Work on ClientLib::TuningConnection tests.

--- Test/ClientTests/TuningConnectionTests.cpp
+++ Test/ClientTests/TuningConnectionTests.cpp
@@ -34,7 +34,20 @@ protected:
 			}
 		};
 
+	inline static std::vector<SoftwareEndpoint::TuningService> s_safeTuningServices =
+		{
+			{
+				.equipmentId = "SYSTEMID_CLIENTTEST_WS04_TUNS",
+				.shortenId = "WS04_TUNS",
+				.clientRequestAddress = {"127.0.0.1", 13335},
+				.drivenSources = {},
+				.singleLmControl = true
+			}
+		};
+
 	inline static const SoftwareInfo s_softwareInfo = {E::SoftwareType::TuningClient, "SYSTEMID_CLIENTTEST_WS03_TUN", 1, 2, 3};
+	inline static const SoftwareInfo s_safeSoftwareInfoA = {E::SoftwareType::TuningClient, "SYSTEMID_CLIENTTEST_WS04_TUNA", 1, 2, 3};
+	inline static const SoftwareInfo s_safeSoftwareInfoB = {E::SoftwareType::TuningClient, "SYSTEMID_CLIENTTEST_WS04_TUNB", 1, 2, 3};
 };
 
 
@@ -132,6 +145,155 @@ TEST_F(TuningConnectionTests, connect)
 
 		EXPECT_EQ(connStates[0].peerAddr, s_tuningServices[0].clientRequestAddress);
 		EXPECT_EQ(connStates[1].peerAddr, s_tuningServices[1].clientRequestAddress);
+	}
+
+	return;
+}
+
+TEST_F(TuningConnectionTests, tuningSourceInfo)
+{
+	ILogFileStub logFile;
+
+	TuningSignalManager signalManager{s_safeSoftwareInfoA.equipmentID(), &logFile};
+
+	ClientLib::TuningUserManager userManager;
+	TuningLog::TuningLogStub tuningLog{userManager, {}};
+
+	Hash lmHash = {::calcHash(QStringLiteral("SYSTEMID_CLIENTTEST_CH12_MD00"))};
+
+	{
+		ClientLib::TuningConnection tc{signalManager, signalManager, &logFile, &tuningLog};
+		tc.updateConnections(s_safeSoftwareInfoA, s_safeTuningServices, true, TuningClientSettings::LmStatusFlagMode::None);
+
+		// Wait for connection established
+		//
+		{
+			QElapsedTimer timer;
+			timer.start();
+
+			while (timer.hasExpired(5000) == false)
+			{
+				QCoreApplication::instance()->processEvents();
+				QThread::msleep(10);
+
+				// Wait for several replies
+				//
+				std::vector<Tcp::ConnectionState> connStates = tc.tcpTuningConnStates();
+				if (std::all_of(connStates.begin(), connStates.end(), [](const auto& s) { return s.isConnected && s.replyCount > 2; }))
+				{
+					break;
+				}
+			}
+		}
+
+		// Check that one connection is established.
+		//
+		std::vector<Tcp::ConnectionState> connStates = tc.tcpTuningConnStates();
+
+		ASSERT_EQ(connStates.size(), 1);
+
+		EXPECT_TRUE(connStates[0].isConnected);
+
+		EXPECT_EQ(connStates[0].peerAddr, s_safeTuningServices[0].clientRequestAddress);
+
+		// Check that tuning source info is arrived
+
+		std::vector<ClientLib::TuningSource> allSourcesInfo = tc.tuningSourcesInfo();
+		EXPECT_EQ(allSourcesInfo.size(), 1);
+
+		std::vector<ClientLib::TuningSource> sourceInfo = tc.tuningSourceInfo(lmHash);
+		EXPECT_EQ(sourceInfo.size(), 1);
+
+		for (const ClientLib::TuningSource& si : sourceInfo)
+		{
+			// Check the contents of TuningSource
+
+			EXPECT_TRUE(si.id() != 0);
+			EXPECT_EQ(si.valid(), true);
+
+			EXPECT_EQ(lmHash, ::calcHash(si.equipmentId()));
+
+			EXPECT_EQ(si.controllersCount(), 1);
+			EXPECT_EQ(si.controllerEquipmentId(0), "SYSTEMID_CLIENTTEST_CH12_MD00_ETHERNET02");
+
+			EXPECT_EQ(si.statesCount(), 1);
+
+			const ::Network::TuningSourceState& st1 = si.state(0);
+			const ::Network::TuningSourceState& st2 = si.state(::calcHash(si.controllerEquipmentId(0)));
+
+			EXPECT_EQ(st1.sourceid(), st2.sourceid());
+
+			EXPECT_FALSE(st1.isreply());	// Sources are not active, so isReply should be false
+			EXPECT_FALSE(st2.isreply());
+		}
+
+		// Check source activation functions
+
+		EXPECT_EQ(tc.tuningSourceStatesCount(lmHash), 1);
+
+		EXPECT_EQ(tc.activatedTuningSourceStatesCount(lmHash), 0);	// No sources are active
+
+		EXPECT_TRUE(tc.activateTuningSource(lmHash, true));
+
+		// Wait for source is activated
+		//
+		{
+			QElapsedTimer timer;
+			timer.start();
+
+			while (timer.hasExpired(5000) == false)
+			{
+				QCoreApplication::instance()->processEvents();
+				QThread::msleep(10);
+
+				if (tc.activatedTuningSourceStatesCount(lmHash) == 1)
+				{
+					break;
+				}
+			}
+		}
+
+		// Check that source has been activated
+		//
+		sourceInfo = tc.tuningSourceInfo(lmHash);
+		EXPECT_EQ(sourceInfo.size(), 1);
+
+		for (const ClientLib::TuningSource& si : sourceInfo)
+		{
+			const ::Network::TuningSourceState& st = si.state(::calcHash(si.controllerEquipmentId(0)));
+			EXPECT_TRUE(st.isreply());
+		}
+
+		EXPECT_TRUE(tc.activateTuningSource(lmHash, false));
+
+		// Wait for source is deactivated
+		//
+		{
+			QElapsedTimer timer;
+			timer.start();
+
+			while (timer.hasExpired(5000) == false)
+			{
+				QCoreApplication::instance()->processEvents();
+				QThread::msleep(10);
+
+				if (tc.activatedTuningSourceStatesCount(lmHash) == 0)
+				{
+					break;
+				}
+			}
+		}
+
+		// Check that source has been deactivated
+		//
+		sourceInfo = tc.tuningSourceInfo(lmHash);
+		EXPECT_EQ(sourceInfo.size(), 1);
+
+		for (const ClientLib::TuningSource& si : sourceInfo)
+		{
+			const ::Network::TuningSourceState& st = si.state(::calcHash(si.controllerEquipmentId(0)));
+			EXPECT_TRUE(st.isreply() == false);
+		}
 	}
 
 	return;
