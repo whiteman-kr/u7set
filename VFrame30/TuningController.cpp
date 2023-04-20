a50b8a62c430cf2600a8a1ab62a47dbbbabc23d8
Boris Vintenko <boris_vin@ukr.net>
2023-04-20 14:53:06 +0300

    RPCT-3514, TuningClient: added signalExists, isAnalog, precision functions to TuningController

--- VFrame30/TuningController.cpp
+++ VFrame30/TuningController.cpp
@@ -61,6 +61,66 @@ namespace VFrame30
 		return result;
 	}
 
+	bool TuningController::signalExists(QString signalId) const
+	{
+		if (m_signalManager == nullptr)
+		{
+			assert(m_signalManager);
+			return {};
+		}
+
+		return m_signalManager->signalExists(::calcHash(signalId));
+	}
+
+	bool TuningController::isDiscrete(QString signalId) const
+	{
+		if (m_signalManager == nullptr)
+		{
+			assert(m_signalManager);
+			return {};
+		}
+
+		bool ok = false;
+
+		AppSignalParam asp = m_signalManager->signalParam(::calcHash(signalId), &ok);
+
+		return ok ?
+					(asp.type() == E::SignalType::Discrete) :
+					false;
+	}
+
+	bool TuningController::isAnalog(QString signalId) const
+	{
+		if (m_signalManager == nullptr)
+		{
+			assert(m_signalManager);
+			return {};
+		}
+
+		bool ok = false;
+
+		AppSignalParam asp = m_signalManager->signalParam(::calcHash(signalId), &ok);
+
+		return ok ?
+					(asp.type() == E::SignalType::Analog) :
+					false;
+	}
+
+	int TuningController::precision(QString signalId) const
+	{
+		if (m_signalManager == nullptr)
+		{
+			assert(m_signalManager);
+			return {};
+		}
+
+		bool ok = false;
+
+		AppSignalParam asp = m_signalManager->signalParam(::calcHash(signalId), &ok);
+
+		return (ok == true && asp.isAnalog() == true) ? asp.precision() : 0;
+	}
+
 	QStringList TuningController::signalIdsByTag(QString tag) const
 	{
 		if (m_signalManager == nullptr)
