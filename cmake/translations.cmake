9789305e8e795b175bba4b7a0328aebfbcb67536
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2023-08-16 11:43:23 +0300

    RPCT-3534, CMake: Optional update translation .ts files.

--- cmake/translations.cmake
+++ cmake/translations.cmake
@@ -1,13 +1,16 @@
 cmake_minimum_required(VERSION 3.16)
 
-function(add_translation target cmake_command output_directory)
+function(add_translation target output_directory)
     qt_add_lupdate(${target} TS_FILES translations/${TARGET}_uk.ts)
-    add_dependencies(${target} ${target}_lupdate)
+
+    if(UPDATE_TRANSLATIONS)
+        add_dependencies(${target} ${target}_lupdate)
+    endif()
 	
     qt_add_lrelease(${target}
 	TS_FILES translations/${target}_uk.ts
         QM_FILES_OUTPUT_VARIABLE qm_files)
 	
-    add_custom_command(TARGET ${target} POST_BUILD COMMAND ${cmake_command} -E make_directory ${output_directory}/translations)
-    add_custom_command(TARGET ${target} POST_BUILD COMMAND ${cmake_command} -E copy ${qm_files} ${output_directory}/translations)
+    add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E make_directory ${output_directory}/translations)
+    add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${qm_files} ${output_directory}/translations)
 endfunction()
