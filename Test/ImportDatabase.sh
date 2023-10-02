84484f2d487d4b34c6285be675a1a98e6a89260c
Boris Vintenko <boris_vin@ukr.net>
2023-10-02 14:29:57 +0000

    Update ImportDatabase.sh to access PostgreSQL by port 5433

--- Test/ImportDatabase.sh
+++ Test/ImportDatabase.sh
@@ -13,12 +13,12 @@ else
     exit 1
 fi
 
-if psql -lqt | cut -d \| -f 1 | grep -qw $2; then
+if psql -lqt --port 5433 | cut -d \| -f 1 | grep -qw $2; then
     echo Database $2 already exists, deleting it.
-    psql -U postgres -c "DROP DATABASE $2;"
+    psql -U postgres --port 5433 -c "DROP DATABASE $2;"
 fi
 
 echo Importing database $2.
 
-psql -U postgres -c "CREATE DATABASE $2 OWNER u7;"
+psql -U postgres --port 5433 -c "CREATE DATABASE $2 OWNER u7;"
 time psql $2 < $1
