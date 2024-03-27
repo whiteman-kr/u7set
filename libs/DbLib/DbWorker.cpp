842f16aab4122d0c6ef75747f9f129f7e76f1ba7
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2024-03-27 12:37:29 +0100

    RPCT-3856, u7: Updated Preset Monitor, set default property SchemaTags to 'AppLogic Monitor'.

--- libs/DbLib/DbWorker.cpp
+++ libs/DbLib/DbWorker.cpp
@@ -442,6 +442,7 @@ const UpgradeItem DbWorker::upgradeItems[] =
 	{":/DatabaseUpgrade/Upgrade0413.sql", "Upgrade to version 413, Add DiagLANDataSize to all LMs, config script has been changed to use this value"},
 	{":/DatabaseUpgrade/Upgrade0414.sql", "Upgrade to version 414, Added function api.undo_changes_recursively"},
 	{":/DatabaseUpgrade/Upgrade0415.sql", "Upgrade to version 415, TestSuite preset update, add FontBold and Section layout to report template"},
+	{":/DatabaseUpgrade/Upgrade0416.sql", "Upgrade to version 416, Updated preset Monitor to version 6, set default value property SchemaTags to: AppLogic Monitor"},
 };
 
 int DbWorker::counter = 0;
