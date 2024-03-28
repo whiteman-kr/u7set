0f4728a7566c7ff22582b56d7806aa5fa142d834
Boris Vintenko <boris_vin@ukr.net>
2024-03-28 08:10:30 +0200

    RPCT-3857, TestSuite: Add an error message appears when trying to save an empty test log

--- TestSuite/TestSuiteMainWindow.cpp
+++ TestSuite/TestSuiteMainWindow.cpp
@@ -754,8 +754,14 @@ bool TestSuiteMainWindow::loadTestLog()
 	return true;
 }
 
-bool TestSuiteMainWindow::saveTestLog()
+void TestSuiteMainWindow::saveTestLog()
 {
+	if (m_testSuite.testLog().empty() == true)
+	{
+		QMessageBox::warning(this, qAppName(), tr("Test log is empty. No information could be saved."));
+		return;
+	}
+
 	QString defaultFileName = QString("TestLog_%1.tsl").arg(QDateTime::currentDateTime().toString("ddMMyyyy_HHmmss"));
 
 	QString fileName = QFileDialog::getSaveFileName(this,
@@ -765,7 +771,7 @@ bool TestSuiteMainWindow::saveTestLog()
 
 	if (fileName.isEmpty() == true)
 	{
-		return false;
+		return;
 	}
 
 	QString errorMsg;
@@ -773,10 +779,10 @@ bool TestSuiteMainWindow::saveTestLog()
 	if (ok == false)
 	{
 		QMessageBox::critical(this, qAppName(), errorMsg);
-		return false;
+		return;
 	}
 
-	return true;
+	return;
 }
 
 void TestSuiteMainWindow::updateStatusIndicator()
