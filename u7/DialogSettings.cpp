a56fa257620a295d7a754717583efc0a1e71d7d9
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2025-03-19 12:00:08 +0100

    RPCT-4001, u7: Added explanatory text for pg_dump/psql in the settings dialog.

--- u7/DialogSettings.cpp
+++ u7/DialogSettings.cpp
@@ -61,7 +61,7 @@ void DialogSettings::showEvent(QShowEvent*)
 	// Resize depends on monitor size, DPI, resolution
 	//
 	QRect screen = parentWidget()->screen()->availableGeometry();
-	resize(static_cast<int>(screen.width() * 0.23), height());
+	resize(static_cast<int>(screen.width() * 0.25), height());
 
 	move(screen.center() - rect().center());
 
