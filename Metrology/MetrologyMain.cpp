9320b7b0b5f1514c46aa1b738b5769fc210f5296
Ovcharenko <ovcharenkoos@gmail.com>
2025-09-03 09:28:09 +0300

    RPCT-4059: moving translation to a separate function

--- Metrology/MetrologyMain.cpp
+++ Metrology/MetrologyMain.cpp
@@ -5,6 +5,9 @@
 #include "version.h"
 #include "../UtilsLib/CrashExceptionHandler.h"
 
+
+static void loadTranslators();
+
 int main(int argc, char* argv[])
 {
 	Vld::setVldReportFilterHook();
@@ -30,47 +33,8 @@ int main(int argc, char* argv[])
 
 	// select language
 	//
-	QTranslator translator;
 
-	// Ovcharenko 01.09.25 "addition of the Ukrainian language"
-
-	static const QMap<OT::LanguageType, QString> langMap = {
-		{OT::LanguageType::English, "en"},
-		{OT::LanguageType::Russian, "ru"},
-		{OT::LanguageType::Ukrainian, "uk"}};
-
-	OT::LanguageType langType = theOptions.language().languageType();
-
-	static QTranslator translatorMetrology;
-	static QTranslator translatorUiLib;
-
-	if (langMap.contains(langType) && langType != OT::LanguageType::English)
-	{
-		QString suffix = langMap.value(langType);
-
-		QString metrologyFile = QApplication::applicationDirPath() + "/translations/Metrology_" + suffix + ".qm";
-		if (translatorMetrology.load(metrologyFile))
-			qApp->installTranslator(&translatorMetrology);
-		else
-			QMessageBox::warning(nullptr,
-								 QObject::tr("Language load error"),
-								 QString("Didn't load Metrology language file:\n%1").arg(metrologyFile));
-
-		if (langType != OT::LanguageType::Russian)
-		{
-			QString uiLibFile = QApplication::applicationDirPath() + "/translations/UiLib_" + suffix + ".qm";
-			if (translatorUiLib.load(uiLibFile))
-				qApp->installTranslator(&translatorUiLib);
-			else
-				QMessageBox::warning(nullptr,
-									 QObject::tr("Language load error"),
-									 QString("Didn't load UiLib language file:\n%1").arg(uiLibFile));
-		}
-	}
-	else
-	{
-		qDebug() << "Using default English, no translator loaded.";
-	}
+	loadTranslators();
 
 	// one instance of the application
 	//
@@ -105,3 +69,57 @@ int main(int argc, char* argv[])
 }
 
 // -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+
+// Language initialization
+// Ovcharenko 02.09.25 "addition of the Ukrainian language"
+// -------------------------------------------------------------------------------------------------------------------
+static void loadTranslators()
+{
+	// 
+
+	static const std::map<OT::LanguageType, QString> langMap = {{OT::LanguageType::English, "en"},
+																{OT::LanguageType::Russian, "ru"},
+																{OT::LanguageType::Ukrainian, "uk"}};
+
+	OT::LanguageType langType = theOptions.language().languageType();
+
+	static QTranslator translatorMetrology;
+	static QTranslator translatorUiLib;
+
+	if (langMap.find(langType) != langMap.end() && langType != OT::LanguageType::English)
+	{
+		QString suffix = langMap.at(langType);
+
+		QString metrologyFile = QApplication::applicationDirPath() + "/translations/Metrology_" + suffix + ".qm";
+		if (translatorMetrology.load(metrologyFile))
+		{
+			qApp->installTranslator(&translatorMetrology);
+		}
+		else
+		{
+			QMessageBox::warning(nullptr,
+								 QObject::tr("Language load error"),
+								 QString("Didn't load Metrology language file:\n%1").arg(metrologyFile));
+		}
+
+		if (langType != OT::LanguageType::Russian)
+		{
+			QString uiLibFile = QApplication::applicationDirPath() + "/translations/UiLib_" + suffix + ".qm";
+			if (translatorUiLib.load(uiLibFile))
+			{
+				qApp->installTranslator(&translatorUiLib);
+			}
+			else
+			{
+				QMessageBox::warning(nullptr,
+									 QObject::tr("Language load error"),
+									 QString("Didn't load UiLib language file:\n%1").arg(uiLibFile));
+			}
+		}
+	}
+	else
+	{
+		qDebug() << "Using default English, no translator loaded.";
+	}
+}
