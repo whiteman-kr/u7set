2f8efa20ea568cb4a6a7edbe0f08e31c9647c0de
Serhiy Malokhatko <serhiy.malokhatko@radiy.com>
2026-05-06 18:44:44 +0200

    RPCT-4143, Tests: Tuning TunigGatewayTests.

--- libs/GatewayClientLib/src/TuningGwConnImpl.cpp
+++ libs/GatewayClientLib/src/TuningGwConnImpl.cpp
@@ -375,7 +375,7 @@ namespace GatewayClientLib
 					break;
 				}
 
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
 				count++;
 			}
 
