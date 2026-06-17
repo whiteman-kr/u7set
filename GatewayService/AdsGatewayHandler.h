02f716e21084ffacac2659fd80862fe16914b858
Yuriy Beliy <whiteman.kr@gmail.com>
2026-06-17 17:49:24 +0300

    RPCT-4140: Obsolete code removing.

--- GatewayService/AdsGatewayHandler.h
+++ GatewayService/AdsGatewayHandler.h
@@ -30,22 +30,10 @@ namespace Gateway
 		virtual void run() override;
 		virtual void shutdown() override;
 
-		virtual void onAppDataSrvConnected() override;
-		virtual void onAppDataSrvDisconnected() override;
-		virtual void planNextPreparedRequest(PreparedRequest& request) override;
-
 		virtual void getRequiredSignalsHashes(std::set<Hash>* hashes) const override;
 		virtual void getEventSignalsHashes(std::set<Hash>* hashes) const override;
 
-		void updateAppSignalStates(const Grpc::GetAppSignalStateReply& reply);
-		void processAppSignalStateChanges(const Grpc::GetAppSignalStateChangesReply& reply);
-		void invalidateSignals();
-
 	private:
-		virtual void prepareRequests() override;
-
-		virtual void runAppDataSrvClient() override;
-
 		void runAdsGatewayServer();
 		void stopAdsGatewayServer();
 
@@ -59,20 +47,4 @@ namespace Gateway
 	};
 
 	using AdsGatewayHandlerShared = std::shared_ptr<AdsGatewayHandler>;
-
-	class AdsGatewayAppSignalStateUpdater : public IAppSignalStateUpdater
-	{
-	public:
-		AdsGatewayAppSignalStateUpdater(AdsGatewayHandler& handler);
-
-		virtual void adsConnected() override;
-		virtual void adsDisconnected() override;
-
-		virtual void updateAppSignalStates(const Grpc::GetAppSignalStateReply& reply) override;
-		virtual void processAppSignalStateChanges(const Grpc::GetAppSignalStateChangesReply& reply) override;
-		virtual void processGatewayAppSignalStateChanges(const Grpc::GetGatewayAppSignalStateChangesReply& reply) override;
-
-	private:
-		AdsGatewayHandler& m_handler;
-	};
 }
